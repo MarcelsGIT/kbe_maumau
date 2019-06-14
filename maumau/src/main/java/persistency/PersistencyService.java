b02a1b88e9731b6c09045ef4c50634dd4e130766
Marcel <deckermarcel91@gmail.com>
2019-06-14 18:20:35 +0200

    new persistency component

---
+++ maumau/src/main/java/persistency/PersistencyService.java
@@ -0,0 +1,5 @@
+package persistency;
+
+public interface PersistencyService {
+
+}
---
+++ maumau/src/main/java/persistency/management/PersistencyMgmt.java
@@ -0,0 +1,7 @@
+package persistency.management;
+
+import persistency.PersistencyService;
+
+public class PersistencyMgmt implements PersistencyService {
+
+}
