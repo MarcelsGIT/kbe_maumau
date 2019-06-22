4728f2f518431ef5b0e158b222e9a4746682655a
Marcel <deckermarcel91@gmail.com>
2019-06-22 21:59:20 +0200

    exception created

---
+++ maumau/src/main/java/util/exceptions/DbConnectionException.java
@@ -0,0 +1,30 @@
+package util.exceptions;
+
+public class DbConnectionException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -437705939189955251L;
+	
+	public DbConnectionException() {
+		
+	}
+	
+	public void printFailureMessage() {
+		System.out.println("Error ID: " + serialVersionUID);
+		System.out.println("Cannot connect to Db");
+		System.out.println("Check Db Creds");
+	}
+	
+	public void printCustomFailureMessage(String message) {
+		System.out.println(message);
+	}
+	
+	public void printCustomFailureMessages(String ... messages){
+		for(String message : messages) {
+			System.out.println(message);
+		}
+	}
+
+}
